20d4d649c85bbc383c866d77cb5c9125c4c5c6dc
jibiel <jibiel@live.com>
2011-08-11 22:41:24 +0400

    added copying rake tasks to application lib/tasks directory and removing ones from plugin directory to avoid  DEPRECATION WARNING message

--- install.rb
+++ install.rb
@@ -3,6 +3,11 @@
 puts "Copying files..."
 
 public_dir = RAILS_ROOT + '/public'
+tasks_dir = RAILS_ROOT + '/lib/tasks/'
+
+tasks = Dir[File.join(File.dirname(__FILE__), '/tasks/*')]
+FileUtils.cp(tasks, tasks_dir)
+FileUtils.rm tasks
 
 scripts = Dir[File.join(File.dirname(__FILE__), '/javascripts/*')]
 FileUtils.cp(scripts, public_dir + '/javascripts/')
